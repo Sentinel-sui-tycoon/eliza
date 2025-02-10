774429b7e212286251e61f10fcce5ac611966214
Jovian Dsouza <dsouzajovian123@gmail.com>
2025-02-10 19:59:38 +0530

    feat: add trade evaluator for sui

--- packages/plugin-sui/src/index.ts
+++ packages/plugin-sui/src/index.ts
@@ -3,6 +3,7 @@ import transferToken from "./actions/transfer.ts";
 import { WalletProvider, walletProvider } from "./providers/wallet.ts";
 import { SuiService } from "./services/sui.ts";
 import swapToken from "./actions/swap.ts";
+import { tradeEvaluator } from "./evaluators/tradeEvaluator.ts";
 
 export { WalletProvider, transferToken as TransferSuiToken };
 
@@ -10,7 +11,7 @@ export const suiPlugin: Plugin = {
     name: "sui",
     description: "Sui Plugin for Eliza",
     actions: [transferToken, swapToken],
-    evaluators: [],
+    evaluators: [tradeEvaluator],
     providers: [walletProvider],
     services: [new SuiService()],
 };
